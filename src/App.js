3695fdd935ad233c64cb06228e1976fe7e6150d7
RISHAB-182001 <57083330+RISHAB-182001@users.noreply.github.com>
2022-01-30 13:45:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import NavBar from './components/NavBar/NavBar.js' // Import NavBar component
+import About from './components/About/About.js'
+import Home from './components/Home/Home.js' // Import Home component
+import Skills from './components/Skills/Skills.js' // Import Skills component
+//import YouTube from './components/YouTube/YouTube.js'
+import Contact from './components/Contact/Contact.js'
 
-function App() {
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      < NavBar />
+      < Home / >
+      < About />
+      < Skills />
+      < Contact />
     </div>
   );
 }
