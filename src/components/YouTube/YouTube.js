155544e8093ac99a80c3aa7a28a9466ebbfac54b
RISHAB-182001 <57083330+RISHAB-182001@users.noreply.github.com>
2022-01-30 13:46:47 +0530

    Create YouTube.js

---
+++ src/components/YouTube/YouTube.js
@@ -0,0 +1,44 @@
+/*
+import classes from './YouTube.module.css'
+import PageHeader from './../PageHeader/PageHeader'
+import icon from './../../img/MyWebsiteLogo.png'
+
+const youtubeVideo=(url, thumbnail, description)=>{
+    return(
+        <div className={classes.VideoContainer}>
+            <div className={classes.Thumbnail}>
+                <img src={thumbnail} alt="thumbnail"/>
+            </div>
+            <div className={classes.Text}>
+                <p>{description}</p>
+            </div>
+        </div>
+    );
+};
+
+const iconDescription = `The Most Interesting and Unique Portfolio Website Pages!`
+
+const Youtube = () => {
+    return ( 
+        <div className={classes.YouTube}>
+            <PageHeader title={"For when I make a YouTube Channel :)"} />
+                <div className={classes.YouTubeContent}>
+                <div className={classes.Paragraph}>
+                    <p>
+                        If I start a YouTube Channel in the future and
+                        the scope is within this resume, I'm going to add 
+                        clip those videos here.
+                    </p>
+                </div>
+                <div className={classes.Videos}>
+                    {
+                        youtubeVideo("https://www.youtube.com/watch?v=0o_JVcSKxRA&list=PLGHxS40mEC9DAjMwg9YqCP6eGzZF0fmEy&index=3"), icon, iconDescription
+                    }
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default Youtube;
+*/
